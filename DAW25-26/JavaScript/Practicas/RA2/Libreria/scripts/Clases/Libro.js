fac1f1b2312ca9f3f882219c1e7da2e0959139e5
Aimar-clase <amerodoa01@larioja.edu.es>
2025-10-27 21:45:42 +0100

    js

---
+++ DAW25-26/JavaScript/Practicas/RA2/Libreria/scripts/Clases/Libro.js
@@ -0,0 +1,17 @@
+
+export class Libro {
+    
+    constructor(nombre, numPaginas, prestado) {
+        this.nombre = nombre;
+        this.numPaginas = numPaginas;
+        this.prestado = prestado;
+    }
+
+    // darDatos() {
+    //     arrayDatos = [this.nombre, this.numPaginas, this.prestado];
+    //     return arrayDatos;
+    // }
+
+
+
+}
---
+++ DAW25-26/JavaScript/Practicas/RA2/Libreria/scripts/recibirDatos.js
@@ -0,0 +1,21 @@
+import {Libro} from './Clases/Libro.js'
+
+let btnEnviar = document.getElementById("btnEnviarDatos");
+let inputNombreLibro = document.getElementById("inputNombre");
+let inputNumPaginas = document.getElementById("inputNumPaginas");
+let inputPrestado = document.getElementById("inputPrestado");
+
+btnEnviar.addEventListener("click", function(){
+
+    let libro = new Libro();
+    let nombreLibro = inputNombreLibro.value;
+
+    let numPaginas = inputNumPaginas.value;
+    let prestado = inputPrestado.value;
+    
+    libro.nombre = nombreLibro;
+    libro.numPaginas = numPaginas;
+    libro.prestado = prestado;
+    alert(libro.nombre + " " + libro.numPaginas + " " + libro.prestado);
+
+})
