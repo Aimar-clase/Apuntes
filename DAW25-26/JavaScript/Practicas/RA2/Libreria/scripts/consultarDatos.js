40328a4bc26b96baca6e53b713d3a0fac6d62c25
Aimar <aimarmerodoa@gmail.com>
2025-11-03 23:22:48 +0100

    JS

--- DAW25-26/JavaScript/Practicas/RA2/Libreria/scripts/consultarDatos.js
+++ DAW25-26/JavaScript/Practicas/RA2/Libreria/scripts/consultarDatos.js
@@ -1,14 +1,32 @@
+const seccionLibros = document.getElementById("seccionLibros");
+addEventListener("DOMContentLoaded", function () {
 
-let contenedorLibros = document.getElementById("contenedorLibros");
+    let datos = JSON.parse(localStorage.getItem('libros'));
 
-addEventListener("DOMContentLoaded", function(){
 
-    let datos = JSON.parse(localStorage.getItem('libro'));
-    let parrafo = document.createElement("p");
-    contenedorLibros.appendChild(parrafo);
-    
-    let tabla = document.createElement("table");
+    datos.forEach(function (libro) {
+        const divLibro = document.createElement("div");
+        seccionLibros.appendChild(divLibro);
+        divLibro.classList.add("divLibro");
 
-    alert(datos.nombre);
+        const TituloLibro = document.createElement("h2");
+        divLibro.appendChild(TituloLibro);
+        TituloLibro.textContent = libro.nombre;
+        TituloLibro.classList.add("TituloLibro");
+
+        const numeroPaginas = document.createElement("p");
+        divLibro.appendChild(numeroPaginas);
+        numeroPaginas.textContent = "Numero de paginas: " + libro.numPaginas;
+        numeroPaginas.classList.add("numeroPaginas");
+
+        const prestado = document.createElement("p");
+        divLibro.appendChild(prestado);
+        prestado.textContent = "¿Ha sido prestado? " + libro.prestado;
+        prestado.classList.add("prestado");
+    });
+
+
+
+
 
 })
--- DAW25-26/JavaScript/Practicas/RA2/Libreria/scripts/recibirDatos.js
+++ DAW25-26/JavaScript/Practicas/RA2/Libreria/scripts/recibirDatos.js
@@ -1,20 +1,21 @@
-import {Libro} from './Clases/Libro.js'
+import { Libro } from './Clases/Libro.js'
 
 let btnEnviar = document.getElementById("btnEnviarDatos");
 let inputNombreLibro = document.getElementById("inputNombre");
 let inputNumPaginas = document.getElementById("inputNumPaginas");
 let inputPrestado = document.getElementById("inputPrestado");
 
-btnEnviar.addEventListener("click", function(){
+btnEnviar.addEventListener("click", function () {
 
-    let libro = new Libro();
+
     let nombreLibro = inputNombreLibro.value;
 
     let numPaginas = inputNumPaginas.value;
     let prestado = inputPrestado.checked;
-    
-    libro.nombre = nombreLibro;
-    libro.numPaginas = numPaginas;
-    libro.prestado = prestado;
-    localStorage.setItem('libro', JSON.stringify(libro))
+    let libro = new Libro(nombreLibro, numPaginas, prestado);
+
+    let libros = JSON.parse(localStorage.getItem('libros')) || [];
+    libros.push(libro);
+
+    localStorage.setItem('libros', JSON.stringify(libros));
 })
